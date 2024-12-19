5db2e4736cd56f87408c4f255807ecd68969d31c
Aperaine <Aperaine@users.noreply.github.com>
2024-12-19 19:04:35 +0900

    a

--- clickbaitify.js
+++ clickbaitify.js
@@ -1,15 +1,15 @@
 (new MutationObserver(() =>
 	document.querySelectorAll('ytd-thumbnail yt-image, .ytp-videowall-still-image')
 		.forEach(e => {
-			if (e.classList.contains('mrbeastified')) return;
+			if (e.classList.contains('clickbaitified')) return;
 			let img = document.createElement('IMG');
-			img.src = `https://mrbeastify.com/MrBeastify${Math.floor(Math.random()*36)+1}.png`;
+			img.src = chrome.runtime.getURL(`images/Clickbaitify${Math.floor(Math.random()*5)}.png`,);
 			img.style.position = 'absolute';
 			img.style.width = '100%';
 			img.style.left = 0;
 			img.style.bottom = 0;
 			img.style.objectFit = 'cover';
 			e.append(img);
-			e.classList.add('mrbeastified');
+			e.classList.add('clickbaitified');
 		})
 )).observe(document.querySelector('ytd-page-manager'), {subtree: true, childList: true});
