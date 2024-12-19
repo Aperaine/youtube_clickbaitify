a098ebfcff2c8e37b7cd9306e12e70f0480fdb62
Aperaine <Aperaine@users.noreply.github.com>
2024-12-19 19:11:04 +0900

    more images

--- clickbaitify.js
+++ clickbaitify.js
@@ -3,7 +3,7 @@
 		.forEach(e => {
 			if (e.classList.contains('clickbaitified')) return;
 			let img = document.createElement('IMG');
-			img.src = chrome.runtime.getURL(`images/Clickbaitify${Math.floor(Math.random()*5)}.png`,);
+			img.src = chrome.runtime.getURL(`images/Clickbaitify${Math.floor(Math.random()*7)}.png`,);
 			img.style.position = 'absolute';
 			img.style.width = '100%';
 			img.style.left = 0;
